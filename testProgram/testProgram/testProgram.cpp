40ee3e9ea060334145799b36f73139c7097ae64e
Jakub ZDANOWSKI <z010346j@student.staffs.ac.uk>
2020-10-26 08:00:39 +0000

    testProgram

--- testProgram/testProgram/testProgram.cpp
+++ testProgram/testProgram/testProgram.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "testProgram\n";
+    std::cout << "testProgram\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
